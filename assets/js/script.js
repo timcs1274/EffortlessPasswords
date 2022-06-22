61db40d480fce65a0c430bc60edba31164f9e8fe
timcs <timcs1274@gmail.com>
2022-06-22 18:42:48 -0400

    add var character object and basic window prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 // Assignment code here
+var characters = {
+  upper: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W",
+"X","Y","Z"],
+  lower: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+  "x","y","z"],
+  speical: ["!","@","#","$","%","&","*"],
+  number: ["0","1","2","3","4","5","6","7","8","9"],
+}
 
+function generatePassword(){
+  var userAnswer = true;
+  var answer =  window.prompt("How many")
+  while (user == true){
+  if (answer.length <= 8)
+  console.log(hello);
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
