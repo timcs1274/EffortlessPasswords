dce7b72be1b5af51610a73b3616258ed008f75de
timcs <timcs1274@gmail.com>
2022-06-23 21:05:18 -0400

    add prompt for selecting character categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,22 +13,22 @@ function writePassword() {
 }
 
 
+var upperChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W",
+"X","Y","Z"];
 
+var lowerChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+"x","y","z"];
+
+var speicalChars = ["!","@","#","$","%","&","*"];
+
+var numberChars = ["0","1","2","3","4","5","6","7","8","9"];
 
 
 function generatePassword() {
 
   //variables for all the possible characters
   
-    var upperChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W",
-    "X","Y","Z"];
-
-    var lowerChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
-    "x","y","z"];
-
-    var speicalChars = ["!","@","#","$","%","&","*"];
-
-    var numberChars = ["0","1","2","3","4","5","6","7","8","9"];
+  
   
   
   //User inputs desired password length between 8 and 128
@@ -42,35 +42,46 @@ function generatePassword() {
     }
   
   
-  //variables for when user confirms 
-  var upperConfirm = ("Would you like to include uppercase characters?");
+  //variables for when user confirms which characters to include
+  var upperConfirm = confirm("Would you like to include uppercase characters?");
 
-  var lowerConfirm = ("Would you like to include lowercase characters?");
+  var lowerConfirm = confirm("Would you like to include lowercase characters?");
 
-  var specialConfirm = ("Would you like to include special characters?");
+  var specialConfirm = confirm("Would you like to include special characters?");
 
-  var numberConfirm = ("Would you like to include numbers?");
+  var numberConfirm = confirm("Would you like to include numbers?");
 
-  var charSelector = []
+  var charSelector = "" //[]
 
+//If the user confirms, concat the variables
 
 if (upperConfirm) {
-  charSelector - charSelector.concat(upperConfirm)
+  charSelector = charSelector.concat(upperConfirm)
 }
 
 if (lowerConfirm) {
-  charSelector - charSelector.concat(lowerConfirm)
+  charSelector = charSelector.concat(lowerConfirm)
 }
 
 if (specialConfirm) {
-  charSelector - charSelector.concat(specialConfirm)
+  charSelector = charSelector.concat(specialConfirm)
 }
 
 if (numberConfirm) {
-  charSelector - charSelector.concat(numberConfirm)
+  charSelector = charSelector.concat(numberConfirm)
+}
+
+//if no characters categories are selected, user is prompted to start over
+if (charSelector == 0){  
+  alert ("You must confirm at least 1 category of characters.");
+  return "Press the 'Generate Password' button to start over."//possibly get rid of this  +=
+} else { password = Math.floor(Math.random() * userLengthAnswer);
+  return password
+
 }
 
 
+//return the completed password here!
 }
 
 
